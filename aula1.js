d40725a0e7d84cf203f9ac4e3720c0856a3e2afc
Peterson Portella <109779768+PetersonPortella@users.noreply.github.com>
2024-08-15 23:01:12 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,7 @@
+let data = new Date();
+document.getElementById("dataTela").innerHTML = data;
+
+function clicar(){
+    document.body.style.background = "#111111";
+    document.body.style.color = "white";
+}
